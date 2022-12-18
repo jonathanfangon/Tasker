423258a82eb5b75eb7897f9a101e05a300e3ac7d
Jonathan <jfangon2002@gmail.com>
2022-12-17 22:19:41 -0800

    added some styles and theme but needs fix

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -2,6 +2,7 @@ import React, { Fragment, useState, useEffect } from 'react';
 
 import InputTodo from "./todolist/InputTodo";
 import ListTodos from "./todolist/ListTodos";
+import './theme.css';
 
 
 const Dashboard = ({ setAuth }) => {
@@ -9,6 +10,7 @@ const Dashboard = ({ setAuth }) => {
     const [name, setName] = useState('');
     const [allTodos, setAllTodos] = useState([]);
     const [todosChange, setTodosChange] = useState(false);
+    const [theme, setTheme] = useState('dark');
 
     async function getName() {
         try {
@@ -36,52 +38,25 @@ const Dashboard = ({ setAuth }) => {
         setAuth(false);
     }
 
+    const toggleTheme = () => {
+        if(theme === 'light') {
+            setTheme('dark');
+        } else {
+            setTheme('light');
+        }
+    }
+
     useEffect(() => {
         getName();
         setTodosChange(false);
     }, [todosChange]);
 
+    useEffect(() => {
+        document.body.className = theme;
+    }, [theme]);
+
     return (
         <Fragment>
-            {/* <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                <a class="navbar-brand" href="#">Navbar</a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav mr-auto">
-                        <li class="nav-item active">
-                            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Link</a>
-                        </li>
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            Dropdown
-                            </a>
-                            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="#">Action</a>
-                                <a class="dropdown-item" href="#">Another action</a>
-                            <div class="dropdown-divider"></div>
-                                <a class="dropdown-item" href="#">Something else here</a>
-                            </div>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-                        </li>
-                    </ul>
-                    <form class="form-inline my-2 my-lg-0">
-                        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
-                        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-                    </form>
-                </div>
-            </nav> */}
-
-            {/* <h1 className='text-center my-3'>Dashboard</h1>
-            <h2 className='text-center'>Welcome back {name}!</h2>
-            <button className='btn btn-primary' onClick={e => logout(e)}>Logout</button> */}
             <div>
                 <div className="d-flex mt-5 justify-content-around">
                     <h1>{name}'s Todo List</h1>
@@ -93,6 +68,7 @@ const Dashboard = ({ setAuth }) => {
                 <InputTodo setTodosChange={setTodosChange} />
                 <ListTodos allTodos={allTodos} setTodosChange={setTodosChange} />
             </div>
+            <button onClick={toggleTheme}>Toggle Theme</button>
         </Fragment>
     );
 };
--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -3,16 +3,21 @@ import { Link } from "react-router-dom";
 
 const Landing = () => {
   return (
-    <div className="jumbotron mt-5" id="landing-cont">
+    <div className="my-5" id="landing-cont">
       <h1 className="text-center">Welcome to Jonathan's To-Do List App!</h1>
-      <h2 className="text-center my-5">Login and start making your to-do list</h2>
       <div className="text-center my-5">
-        <Link to="/login" className="btn btn-primary btn-lg">
-        Login
-      </Link>
-      <Link to="/register" className="btn btn-primary ml-3 btn-lg">
-        Register
-      </Link>
+        <div id="login-landing">
+          <h2 className="text-center my-5">Login and start making your to-do list</h2>
+          <Link to="/login" className="btn btn-primary btn-lg">
+            Login
+          </Link>
+        </div>
+        <div id="register-landing">
+          <h3 className="my-5">Don't have an account? Register <Link to="/register">
+              here
+            </Link>
+          </h3>
+        </div>
       </div>
     </div>
   );
